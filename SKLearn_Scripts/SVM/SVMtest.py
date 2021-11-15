7370d61678eed679276de385c62cb268d3e1304c
Liam Cochrane <liam.cochrane@hotmail.co.uk>
2021-11-15 20:16:34 +0000

    implemented basic svm on iris dataset

--- SKLearn_Scripts/SVM/SVMtest.py
+++ SKLearn_Scripts/SVM/SVMtest.py
@@ -1,6 +1,9 @@
 #SVM uses hyperplanes for classification or regression.
 from sklearn import datasets
 from sklearn.model_selection import train_test_split
+from sklearn import neighbors, metrics
+from sklearn import svm
+import joblib
 import numpy as np
 import pandas as pd
 
@@ -13,6 +16,10 @@ classification = ['Iris Setosa', 'Iris Versicolour', 'Iris Virginica']
 
 x_train, x_test, y_train, y_test = train_test_split(x,y,test_size=0.2) #percentage as decimal
 
-model =svm.SVC()
+model = svm.SVC()
 model.fit(x_train,y_train)
+predictions = model.predict(x_test)
+acc=metrics.accuracy_score(y_test,predictions)
 print(model)
+print("accuracy:",acc)
+joblib.dump(model,'./svm.sav')
